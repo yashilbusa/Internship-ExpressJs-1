7a0deaf0ff50004d17440d0402f6414e0267e874
yashilbusa <yashilpatel1914@gmail.com>
2025-02-14 12:19:47 +0530

    commit

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ const app = express();
 const port = 3012;
 
 app.get('/',(req,res)=>{
-    res.end("Hello World");
-    // res.end("And This is Home Page");
+    res.send("<b>Home Page</b>");
 })
+
+app.get('/about',(req,res)=>{
+    res.send("<b>About Page</b>");
+})
+
+app.get('/users',(req,res)=>{
+    res.send("<b>Users Page</b>");
+})
+
 
 app.listen(port,()=>{
     console.log(`Server is Running on http://localhost:${port}`);
