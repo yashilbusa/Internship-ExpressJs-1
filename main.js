5376eeb8463cf36d90f79bb97855a31f51a34574
yashilbusa <yashilpatel1914@gmail.com>
2025-02-14 14:15:39 +0530

    commit

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3012;
 
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.get('/',(req,res)=>{
     res.send("<b>Home Page</b>");
@@ -17,9 +18,20 @@ app.get('/users',(req,res)=>{
     res.send("<b>Users Page</b>");
 })
 
+app.get('/signup',(req,res)=>{
+    const filePath = path.join(__dirname, './public/index.html');
+    res.sendFile(filePath);
+})
+
+
 app.post('/',(req,res)=>{
     res.send("It's a Post Request");
 })
+
+app.put('/',(req,res)=>{
+    res.send("It's a Put Request");
+})
+
 
 app.listen(port,()=>{
     console.log(`Server is Running on http://localhost:${port}`);
