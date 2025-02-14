234238dbc9457f47eca65f7b16d00bbcf9462950
yashilbusa <yashilpatel1914@gmail.com>
2025-02-14 12:06:34 +0530

    commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3012;
 
 app.get('/',(req,res)=>{
     res.end("Hello World");
-    res.end("And This is Home Page");
+    // res.end("And This is Home Page");
 })
 
 app.listen(port,()=>{
