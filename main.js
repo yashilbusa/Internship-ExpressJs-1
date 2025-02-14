58cfbc9c491c06e8a3dbd84cccde2d704b3cdc93
yashilbusa <yashilpatel1914@gmail.com>
2025-02-14 13:06:52 +0530

    commmit

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3012;
 
+
+app.use(express.static("public"));
+
 app.get('/',(req,res)=>{
     res.send("<b>Home Page</b>");
 })
@@ -14,7 +17,10 @@ app.get('/users',(req,res)=>{
     res.send("<b>Users Page</b>");
 })
 
+app.post('/',(req,res)=>{
+    res.send("It's a Post Request");
+})
 
 app.listen(port,()=>{
     console.log(`Server is Running on http://localhost:${port}`);
-})
+}) 
